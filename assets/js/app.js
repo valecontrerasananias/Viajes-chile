5b6cf40e9c0e688a26f02c501540db0da8ece9f0
Vale Contreras <v.contreras.ananias@gmail.com>
2021-12-27 17:13:21 -0300

    ultimos cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,35 @@
+//Tooltip bs//
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+$(document).ready(function () {
+
+//METODO TOGGLE//
+    //torres-del-paine//
+    $(".torres-del-paine").on("click", function () {
+        $(".card-text1").toggle();
+    });
+
+    //los-flamencos//
+    $(".los-flamencos").on("click", function () {
+        $(".card-text2").toggle();
+    });
+
+    //desierto//
+    $(".laguna").on("click", function () {
+        $(".card-text3").toggle();
+    });
+
+    //bosque//
+    $(".bosque").on("click", function () {
+        $(".card-text4").toggle();
+    });
+
+    });
+    
+
+  
